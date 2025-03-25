69f23220b80c28b02d2bb6c274406cefd731696c
Boban Davidovic <boban.da87@gmail.com>
2025-03-25 23:22:57 +0100

    chore: connect to the remote tf state file

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  # storage account settings
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stgtfstate25032025"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
