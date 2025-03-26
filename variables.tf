02748852ef3ac7f3d2427df55e1dcbe2e82a9b5c
Boban Davidovic <boban.da87@gmail.com>
2025-03-26 01:19:00 +0100

    chore: adding vars

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "environment" {
+  description = "The environment (e.g. dev, staging, prod)"
+  type        = string
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "The environment must be one of the following: dev, staging, prod."
+  }
+}
+
+variable "region" {
+  description = "The region where the resources will be created"
+  type        = string
+  default     = "norwayeast"
+
+  validation {
+    condition     = var.region == "norwayeast"
+    error_message = "The region must be 'norwayeast'."
+  }
+}
+
+variable "resource_group_names" {
+  description = "A list of resource group names"
+  type        = list(string)
+  default = [
+    "core",
+    "apps",
+    "shared",
+  ]
+}
+
+variable "tags" {
+  description = "A map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
